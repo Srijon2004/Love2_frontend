2ee4614bb04f2d58b2c1a1c8b9236e2e83da1b32
Srijon2004 <srijonchowdhury6@gmail.com>
2025-08-31 13:37:29 +0530

    D!

--- fronted/src/utils/api.js
+++ fronted/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ matches your backend
+  baseURL: "https://love-backend-7yko.onrender.com/api", // ✅ matches your backend
   withCredentials: true, 
 });
 
