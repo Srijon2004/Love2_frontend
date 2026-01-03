beff4aa2a35863532fbda0a8096c9021a65f3a1a
Srijon2004 <srijonchowdhury6@gmail.com>
2026-01-03 21:33:28 +0530

    Added new updated project code

--- fronted/src/utils/api.js
+++ fronted/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
     // baseURL: "https://love-backend-7yko.onrender.com/api", // ✅ matches your backend
   //  baseURL:"http://localhost:5000/api",
-  baseURL:"https://love2-backend.onrender.com",
+  baseURL:"https://love2-backend.onrender.com/api",
   withCredentials: true, 
 });
 
